7e301169fbd176946af37996fbe3c85b08f47d99
LynnKamau <lkamau7@usiu.ac.ke>
2022-04-05 22:52:18 +0300

    Changed Front Page Files
    Changed some css to make it look good.
    
    I deleted home.hbs didn't need it
    
    Renamed students.json file to reviews. json to reduce similarity with previous project.
    
    Added some fonts and applied them
    
    Isn't aesthetic as much ) : - (

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 const jsonParser = bodyParser.json();
-const fileName = 'students.json';
+const fileName = 'reviews.json';
 
 // Load data from file
 let rawData = fs.readFileSync(fileName);
